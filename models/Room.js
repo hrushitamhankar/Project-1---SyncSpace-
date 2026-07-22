d10ea844594fc6df014f8862014a0dd745eefe05
Jenniskamboj <jenniskamboj@gmail.com>
2026-07-22 23:12:03 +0530

    Add role support for invited users

--- models/Room.js
+++ models/Room.js
@@ -1,29 +1,22 @@
-const mongoose = require("mongoose");
+const roomSchema = new mongoose.Schema({
+  roomId: String,
 
-const roomSchema = new mongoose.Schema(
-  {
-    roomId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 
-    ownerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    invitedUsers: [
-      {
+  invitedUsers: [
+    {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
-
-module.exports = mongoose.model("Room", roomSchema);
+      role: {
+        type: String,
+        enum: ["editor", "viewer"],
+        default: "viewer",
+      },
+    },
+  ],
+});
